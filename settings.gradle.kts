01e1bce71808af3ed2c737164fea038a559b5475
Abhitestitnow <mahapatraabhilash108@gmail.com>
2025-10-08 14:16:08 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,23 @@
 // settings.gradle.kts
+// settings.gradle.kts
+
+pluginManagement {
+    repositories {
+        // Required for Android Gradle Plugin and AndroidX
+        google()
+        // Standard repositories
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "Flashcards"
-include(":app")
+include(":app"))
