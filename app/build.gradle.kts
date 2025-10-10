3c48556e520dd831be0327ab718107262df72d3a
Abhitestitnow <mahapatraabhilash108@gmail.com>
2025-10-10 13:38:36 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0"
 }
 
 android {
@@ -20,7 +21,7 @@ android {
     }
 
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.14"
+        // Remove kotlinCompilerExtensionVersion as Compose compiler plugin handles this now
     }
 
     compileOptions {
