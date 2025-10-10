017efbbd4eda0eadeaecfbaea4470ca9f5503097
Abhitestitnow <mahapatraabhilash108@gmail.com>
2025-10-10 17:53:54 +0530

    Create FlashcardGroup.kt

---
+++ app/src/main/java/com/example/flashcards/data/FlashcardGroup.kt
@@ -0,0 +1,10 @@
+package com.example.flashcards.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "flashcard_groups")
+data class FlashcardGroup(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val name: String
+)
