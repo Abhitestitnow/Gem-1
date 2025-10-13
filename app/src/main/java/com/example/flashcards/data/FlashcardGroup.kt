c7ea716d951b6dc11f3dc17fd52ad7dd09f4125a
Abhitestitnow <mahapatraabhilash108@gmail.com>
2025-10-13 13:40:54 +0530

    Update FlashcardGroup.kt

--- app/src/main/java/com/example/flashcards/data/FlashcardGroup.kt
+++ app/src/main/java/com/example/flashcards/data/FlashcardGroup.kt
@@ -5,6 +5,7 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "flashcard_groups")
 data class FlashcardGroup(
-    @PrimaryKey(autoGenerate = true) val id: Int = 0,
-    val name: String
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val groupName: String
 )
