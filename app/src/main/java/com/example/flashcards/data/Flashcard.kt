59e2d883c24e8d86282713fa389786371821cab1
Abhitestitnow <mahapatraabhilash108@gmail.com>
2025-10-13 15:53:35 +0530

    Update Flashcard.kt

--- app/src/main/java/com/example/flashcards/data/Flashcard.kt
+++ app/src/main/java/com/example/flashcards/data/Flashcard.kt
@@ -2,16 +2,20 @@ package com.example.flashcards.data
 
 import androidx.room.Entity
 import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
 @Entity(
     tableName = "flashcards",
-    foreignKeys = [ForeignKey(
-        entity = FlashcardGroup::class,
-        parentColumns = ["id"],
-        childColumns = ["groupId"],
-        onDelete = ForeignKey.CASCADE
-    )]
+    foreignKeys = [
+        ForeignKey(
+            entity = FlashcardGroup::class,
+            parentColumns = ["id"],
+            childColumns = ["groupId"],
+            onDelete = ForeignKey.CASCADE
+        )
+    ],
+    indices = [Index(value = ["groupId"])]
 )
 data class Flashcard(
     @PrimaryKey(autoGenerate = true)
